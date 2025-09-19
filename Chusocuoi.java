a826449104301b484529cb367dba77bf59bc5041
nguyentuananh2006 <tuananh199720vl@gmail.com>
2025-09-19 16:35:53 +0700

    Add files via upload

---
+++ Chusocuoi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Chusocuoi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số nguyên N: ");
+        int n = sc.nextInt();
+
+        int lastDigit = Math.abs(n % 10);
+
+        System.out.println("Chữ số cuối cùng của " + n + " là: " + lastDigit);
+
+        sc.close();
+    }
+}
